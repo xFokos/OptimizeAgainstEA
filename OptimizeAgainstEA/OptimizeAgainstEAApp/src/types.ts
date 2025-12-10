8dfc68d0d0ab33f3bb80d5dce4e999066972780a
xFokos <sehne.philipp@gmail.com>
2025-12-10 23:30:44 +0100

    Headline Style changes

--- OptimizeAgainstEA/OptimizeAgainstEAApp/src/types.ts
+++ OptimizeAgainstEA/OptimizeAgainstEAApp/src/types.ts
@@ -1,4 +1,4 @@
-import React, { type MouseEvent } from "react";
+
 
 export type Node = {
     id: number;
@@ -11,12 +11,6 @@ export type Edge = {
     from: number; // node id
     to: number;   // node id
 };
-
-// Common props all buttons in the group should support
-export interface SelectableButtonProps {
-    isSelected?: boolean;
-    onClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-}
 
 export interface ButtonConfig<P = any> {
     key?: string | number;
