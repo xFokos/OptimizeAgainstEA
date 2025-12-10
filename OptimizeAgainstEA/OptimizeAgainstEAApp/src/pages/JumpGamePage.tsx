115a01a4d9ac75963fcc947ed9e86f9d484490b0
xFokos <sehne.philipp@gmail.com>
2025-12-10 23:49:26 +0100

    Add Analytics Button to JumpGamePage.tsx

--- OptimizeAgainstEA/OptimizeAgainstEAApp/src/pages/JumpGamePage.tsx
+++ OptimizeAgainstEA/OptimizeAgainstEAApp/src/pages/JumpGamePage.tsx
@@ -1,13 +1,12 @@
 // src/pages/JumpGame.tsx
 import React, { useState, useEffect } from "react";
 import "../styles/JumpGame.css";
-import { useNavigate } from "react-router-dom";
+import NavigatePageButton from "../components/NavigatePageButton.tsx";
 
 
 const JumpGame: React.FC = () => {
     const [isJumping, setIsJumping] = useState<boolean>(false);
     const [jumpCount, setJumpCount] = useState<number>(0);
-    const navigate = useNavigate();
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
@@ -34,7 +33,7 @@ const JumpGame: React.FC = () => {
                 <div className={`player ${isJumping ? "jump" : ""}`}></div>
             </div>
             <p>Sprünge: {jumpCount}</p>
-            <button onClick={() => navigate("/")}>Zurück zur Startseite</button>
+            <NavigatePageButton to={"/Analytics"} text={"View Analytics"} />
         </div>
     );
 };
